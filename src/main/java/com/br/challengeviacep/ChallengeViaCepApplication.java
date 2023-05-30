3c073a7027391d8331eb69e55fb2f4b8a96d3198
Nicolas D <78985734+Deb4cker@users.noreply.github.com>
2023-05-30 10:59:55 -0300

    project started

---
+++ src/main/java/com/br/challengeviacep/ChallengeViaCepApplication.java
@@ -0,0 +1,13 @@
+package com.br.challengeviacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeViaCepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeViaCepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/challengeviacep/ChallengeViaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.challengeviacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeViaCepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
